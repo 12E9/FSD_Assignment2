595c9c4771daf68e21bfaba90014ed4fe6c50a61
Your Name <you@example.com>
2022-02-10 02:36:58 -0600

    added

---
+++ src/Cards.jsx
@@ -0,0 +1,52 @@
+import Faker from "faker";
+function Cards() {
+  return (
+    <div className="ui cards">
+      <div className="card">
+        <div className="content">
+          <img
+            className="right floated mini ui image"
+            src={Faker.image.image()}
+            height={20}
+            width={20}
+            //"/images/avatar/large/elliot.jpg"
+          />
+          <div className="header">Elliot Fu</div>
+          <div className="meta">Friends of Veronika</div>
+          <div className="description">
+            Elliot requested permission to view your contact details
+          </div>
+        </div>
+        <div className="extra content">
+          <div className="ui two buttons">
+            <div className="ui basic green button">Approve</div>
+            <div className="ui basic red button">Decline</div>
+          </div>
+        </div>
+        </div>
+
+        <div className="card">
+          <div className="content">
+            <img
+              className="right floated mini ui image"
+              src={Faker.image.image()}
+              //"/images/avatar/large/jenny.jpg"
+            />
+            <div className="header">Jenny Hess</div>
+            <div className="meta">New Member</div>
+            <div className="description">
+              Jenny wants to add you to the group <b>best friends</b>
+            </div>
+          </div>
+          <div className="extra content">
+            <div className="ui two buttons">
+              <div className="ui basic green button">Approve</div>
+              <div className="ui basic red button">Decline</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  );
+}
+export default Cards;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import Cards from './Cards.jsx';
+
+
+ReactDOM.render(
+    <div>
+        <Cards />
+    </div>
+    ,
+    document.getElementById("root") 
+);
